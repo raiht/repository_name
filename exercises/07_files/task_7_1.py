c46e42b05f5e4c5f93d25667296ddd866c04ba92
raiht <timon.r13@mail.ru>
2023-08-09 06:53:25 +0300

    Всего одна задача.

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,24 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+ospf_file = open('ospf.txt', 'r')
+ospf_source = ospf_file.read()
+ospf_file.close()
+ospf_transformed = [line.split()[1:] for line in ospf_source.split('\n')][:-1]
+
+output_template = '''
+Prefix                {}
+AD/Metric             {}
+Next-Hop              {}
+Last update           {}
+Outbound Interface    {}'''
+
+for line in ospf_transformed:
+    # print(line)
+    prefix = line[0]
+    ad_metric = line[1][1:-1]
+    next_hop = line[3][:-1]
+    last_update = line[4][:-1]
+    outbound_interface = line[5]
+    print(output_template.format(prefix, ad_metric, next_hop, last_update, outbound_interface))
